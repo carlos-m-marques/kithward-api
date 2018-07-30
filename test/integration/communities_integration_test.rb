5d0a11bf66eb13f032da4e882d7bc4017559168d
Sebastian Delmont <sd@notso.net>
2018-07-30 08:41:36 -0400

    communities, not facilities

--- test/integration/communities_integration_test.rb
+++ test/integration/communities_integration_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class CommunitiesIntegrationTest < ActionDispatch::IntegrationTest
   setup do
-    @f1 = create(:community, name: 'Golden Pond', description: 'Excelent Care')
-    @f2 = create(:community, name: 'Silver Lining', description: 'Incredible Care')
-    @f3 = create(:community, name: 'Gray Peaks', description: 'Incredible Service')
+    @c1 = create(:community, name: 'Golden Pond', description: 'Excelent Care')
+    @c2 = create(:community, name: 'Silver Lining', description: 'Incredible Care')
+    @c3 = create(:community, name: 'Gray Peaks', description: 'Incredible Service')
 
     @account = create(:account)
     @admin_account = create(:account, is_admin: true)
@@ -25,26 +25,26 @@ class CommunitiesIntegrationTest < ActionDispatch::IntegrationTest
 
     get "/v1/communities", params: {q: 'Care'}
     assert_response :success
-    assert_equal [@f1.id.to_s, @f2.id.to_s], json_response['data'].collect {|result| result['id']}
-    assert_equal [@f1.name, @f2.name], json_response['data'].collect {|result| result['attributes']['name']}
+    assert_equal [@c1.id.to_s, @c2.id.to_s], json_response['data'].collect {|result| result['id']}
+    assert_equal [@c1.name, @c2.name], json_response['data'].collect {|result| result['attributes']['name']}
 
     get "/v1/communities", params: {q: 'Incredible'}
     assert_response :success
-    assert_equal [@f2.id.to_s, @f3.id.to_s], json_response['data'].collect {|result| result['id']}
-    assert_equal [@f2.name, @f3.name], json_response['data'].collect {|result| result['attributes']['name']}
+    assert_equal [@c2.id.to_s, @c3.id.to_s], json_response['data'].collect {|result| result['id']}
+    assert_equal [@c2.name, @c3.name], json_response['data'].collect {|result| result['attributes']['name']}
 
     get "/v1/communities", params: {q: 'Service'}
     assert_response :success
-    assert_equal [@f3.id.to_s], json_response['data'].collect {|result| result['id']}
-    assert_equal [@f3.name], json_response['data'].collect {|result| result['attributes']['name']}
+    assert_equal [@c3.id.to_s], json_response['data'].collect {|result| result['id']}
+    assert_equal [@c3.name], json_response['data'].collect {|result| result['attributes']['name']}
   end
 
   test "retrieve one" do
-    get "/v1/communities/#{@f1.id}"
+    get "/v1/communities/#{@c1.id}"
     assert_response :success
 
-    assert_equal @f1.id.to_s, json_response['data']['id']
-    assert_equal @f1.name, json_response['data']['attributes']['name']
+    assert_equal @c1.id.to_s, json_response['data']['id']
+    assert_equal @c1.name, json_response['data']['attributes']['name']
   end
 
   test "anonymous users can get all communities" do
@@ -62,40 +62,40 @@ class CommunitiesIntegrationTest < ActionDispatch::IntegrationTest
   end
 
   test "anonymous users cannot update communities" do
-    put "/v1/communities/#{@f1.id}", params: {name: "Updated Community"}
+    put "/v1/communities/#{@c1.id}", params: {name: "Updated Community"}
     assert_response 401 # unauthorized
   end
 
   test "authenticated users cannot update communities" do
-    put "/v1/communities/#{@f1.id}", params: {name: "Updated Community", access_token: @token}
+    put "/v1/communities/#{@c1.id}", params: {name: "Updated Community", access_token: @token}
     assert_response 401 # unauthorized
   end
 
   test "admin users can update communities" do
-    put "/v1/communities/#{@f1.id}", params: {name: "Updated Community", access_token: @admin_token}
+    put "/v1/communities/#{@c1.id}", params: {name: "Updated Community", access_token: @admin_token}
     assert_response :success
 
-    assert_equal @f1.id.to_s, json_response['data']['id']
+    assert_equal @c1.id.to_s, json_response['data']['id']
     assert_equal "Updated Community", json_response['data']['attributes']['name']
 
-    get "/v1/communities/#{@f1.id}"
+    get "/v1/communities/#{@c1.id}"
     assert_response :success
 
-    assert_equal @f1.id.to_s, json_response['data']['id']
+    assert_equal @c1.id.to_s, json_response['data']['id']
     assert_equal "Updated Community", json_response['data']['attributes']['name']
   end
 
   test "admin users can add images to communities" do
-    put "/v1/communities/#{@f1.id}", params: {name: "Updated Community", access_token: @admin_token}
+    put "/v1/communities/#{@c1.id}", params: {name: "Updated Community", access_token: @admin_token}
     assert_response :success
 
-    assert_equal @f1.id.to_s, json_response['data']['id']
+    assert_equal @c1.id.to_s, json_response['data']['id']
     assert_equal "Updated Community", json_response['data']['attributes']['name']
 
-    get "/v1/communities/#{@f1.id}"
+    get "/v1/communities/#{@c1.id}"
     assert_response :success
 
-    assert_equal @f1.id.to_s, json_response['data']['id']
+    assert_equal @c1.id.to_s, json_response['data']['id']
     assert_equal "Updated Community", json_response['data']['attributes']['name']
   end
 end
