8407edc0d6bd0f91cf319f684c4a9024d0978973
Sebastian Delmont <sd@notso.net>
2018-09-21 08:42:38 -0400

    No, really, only run in production

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,7 @@
-if Rails.env.production?
+if Rails.env.production? && ENV['HEROKU_APP_NAME'] == 'kwapi'
+  require 'ddtrace'
+
   Datadog.configure do |c|
-    c.use :rails, service_name: 'kwapi'
+    c.use :rails, service_name: ENV['HEROKU_APP_NAME']
   end
 end
