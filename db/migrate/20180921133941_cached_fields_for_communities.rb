12568ea6357197ebeaedc7e387544f51a5c56c91
Sebastian Delmont <sd@notso.net>
2018-09-22 10:27:57 -0400

    consolidate cache migrations to prevent sequencing errors

--- db/migrate/20180921133941_cached_fields_for_communities.rb
+++ db/migrate/20180921133941_cached_fields_for_communities.rb
@@ -1,10 +1,13 @@
-class CachedThumbnailForCommunities < ActiveRecord::Migration[5.2]
+class CachedFieldsForCommunities < ActiveRecord::Migration[5.2]
   def change
     add_column :communities, :cached_image_url, :string, limit: 128
+    add_column :communities, :cached_data, :jsonb
 
     Community.reset_column_information
     Community.find_each do |c|
       c.update_cached_image_url!
+      c.update_cached_data
+      c.save
     end
   end
 end
