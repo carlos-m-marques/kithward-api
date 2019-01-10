6913679aa32f2b48eb7b1fa9dec23ffff0dfc01f
Sebastian Delmont <sd@notso.net>
2019-01-10 17:17:38 -0500

    disable ip spoofing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_dispatch.ip_spoofing_check = false
 end
