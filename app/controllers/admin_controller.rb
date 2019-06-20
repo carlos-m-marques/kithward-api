e81d2232a9946807410a4908f0fe613ecc12f416
Joao Moraes <joaomoraes@gmail.com>
2019-06-20 17:03:31 -0300

    Fix AdminController#clone_db constant call

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :admin_account_required!
 
   def clone_db
-    if !Rails.env.production? && ENV['HEROKU_APP_NAME'] != PROD_APP_NAME
+    if !Rails.env.production? && ENV['HEROKU_APP_NAME'] != CopyProdDbToStagingService::PROD_APP_NAME
       system("heroku run:detached rails runner 'CopyProdDbToStagingService.run' -a #{ENV['HEROKU_APP_NAME']}") 
       render json: { msg: "Clone production DB to #{ENV['HEROKU_APP_NAME']} environment started. This should take a few minutes." }, status: 200
     else
