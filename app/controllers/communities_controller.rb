aaf9e35ae38bc5f2310c849a5affb072e22b8a7c
Joao Moraes <joaomoraes@gmail.com>
2019-06-21 20:57:13 -0300

    Use request.remote_ip to find ip

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -224,7 +224,7 @@ class CommunitiesController < ApplicationController
   end
 
   def near_by_ip
-    near_by_ip_service = NearByIpService.new(params[:ip], default_search_options)
+    near_by_ip_service = NearByIpService.new(request.remote_ip, default_search_options)
     render json: CommunitySerializer.render(near_by_ip_service.communities.sample(2), view: 'simple')
   end
 
