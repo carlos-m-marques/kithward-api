fe9788d8c8ef4533e2ac9b31ac31906785c263d7
Carlos Mattos <dudufangor@gmail.com>
2019-08-21 16:09:48 -0300

    Listings importer

---
+++ app/workers/listings_importer_worker.rb
@@ -0,0 +1,17 @@
+class ListingsImporterWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'default'
+
+  def perform(listing_id)
+    listing = Listing.select(:id, :community_id, :name).find(listing_id)
+    unit_layout = UnitType.create(name: listing.name, community_id: listing.community_id)
+
+    listing.listing_images.each do |listing_image|
+      unit_layout_image = unit_layout.unit_type_images.new
+      unit_layout_image.image.attach(listing_image.image.blob)
+
+      unit_layout_image.save
+    end
+
+  end
+end
