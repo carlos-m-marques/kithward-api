4bb245b7f7db25b181587b1339e72d6bfa0d3581
Carlos Mattos <dudufangor@gmail.com>
2019-08-23 15:09:49 -0300

    fix

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -112,7 +112,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  config.root_to = 'kw_super_classes#index'
+  config.root_to = 'super_classes#index'
 
   # == Admin Comments
   #
