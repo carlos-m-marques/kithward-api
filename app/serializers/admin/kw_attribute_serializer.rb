64b213b21a09b9fdb4233469fed2fab7be157b00
Carlos Mattos <dudufangor@gmail.com>
2019-08-27 19:03:20 -0300

    ok

--- app/serializers/admin/kw_attribute_serializer.rb
+++ app/serializers/admin/kw_attribute_serializer.rb
@@ -2,7 +2,7 @@ module Admin
   class KwAttributeSerializer < Blueprinter::Base
     identifier :id
 
-    fields :name, :ui_type, :created_at, :updated_at
+    fields :name, :ui_type, :created_at, :updated_at, :required
 
     association :kw_values, name: :values, blueprint: Admin::KwValueSerializer
   end
