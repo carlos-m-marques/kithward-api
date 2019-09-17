c5550f4393db0ea281e4348b32031f4d070ca7ad
Carlos Mattos <dudufangor@gmail.com>
2019-09-17 15:03:39 -0300

    $1

--- db/migrate/20190917180229_add_update_communities_aasm.rb
+++ db/migrate/20190917180229_add_update_communities_aasm.rb
@@ -1,9 +1,9 @@
-class ChangeCommunityStatusValues < ActiveRecord::Migration[5.2]
-  STATUS_ACTIVE    = 'A'
-  STATUS_DRAFT     = '?'
-  STATUS_DELETED   = 'X'
-
+class AddUpdateCommunitiesAasm < ActiveRecord::Migration[5.2]
   def change
+    STATUS_ACTIVE    = 'A'
+    STATUS_DRAFT     = '?'
+    STATUS_DELETED   = 'X'
+
     Community.where(status: STATUS_ACTIVE).update_all(status: Community::STATE_ACTIVE)
     Community.where(status: STATUS_DRAFT).update_all(status: Community::STATE_DRAFT)
     Community.where(status: STATUS_DELETED).each(&:destroy)
